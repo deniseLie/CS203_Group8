b93d0900282d6fe49b8bf704a42650b54e827712
Rachel YJ <rachelyj14@gmail.com>
2024-09-30 13:59:37 +0800

    fixed! now u can see all tournaments
    updated api docs to include this endpoint

--- main/Ranking Ms/Ranking-Ms/src/main/java/csd/backend/Ranking/Ms/TournamentRepository.java
+++ main/Ranking Ms/Ranking-Ms/src/main/java/csd/backend/Ranking/Ms/TournamentRepository.java
@@ -5,7 +5,8 @@ package csd.backend.Ranking.Ms;
 // }
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
+@Repository
 public interface TournamentRepository extends JpaRepository<Tournament, Long> {
     
 }
--- main/Ranking Ms/Ranking-Ms/src/main/java/csd/backend/Ranking/Ms/TournamentService.java
+++ main/Ranking Ms/Ranking-Ms/src/main/java/csd/backend/Ranking/Ms/TournamentService.java
@@ -2,9 +2,18 @@ package csd.backend.Ranking.Ms;
 
 import java.util.*;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TournamentService {
 
     private TournamentRepository tournamentRepository;
+
+    @Autowired 
+    public TournamentService(TournamentRepository tournamentRepository){
+        this.tournamentRepository = tournamentRepository;
+    }
 
     public List<Tournament> getAllTournaments() {
         return tournamentRepository.findAll();

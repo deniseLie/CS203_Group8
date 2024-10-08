4e02fed049f0018f93a01062500ca85c5aae4e44
Rachel YJ <rachelyj14@gmail.com>
2024-10-08 22:37:16 +0800

    initial /getPlayersById
    not updated

--- main/Ranking Ms/Ranking-Ms/src/main/java/csd/backend/Ranking/Ms/TournamentService.java
+++ main/Ranking Ms/Ranking-Ms/src/main/java/csd/backend/Ranking/Ms/TournamentService.java
@@ -4,9 +4,13 @@ import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class TournamentService {
+    @Autowired
+    private RestTemplate restTemplate;
+    private final String MATCHING_MS_BASE_URL = "http://localhost:8081"; 
 
     private TournamentRepository tournamentRepository;
 
@@ -17,6 +21,21 @@ public class TournamentService {
 
     public List<Tournament> getAllTournaments() {
         return tournamentRepository.findAll();
+    }
+
+     
+
+
+    public List<Player> getPlayersByIds(List<Long> playerIds) {
+        List<Player> players = new ArrayList<>();
+
+        for (Long id : playerIds) {
+            String url = MATCHING_MS_BASE_URL + "/getPlayerById/" + id;
+            Player player = restTemplate.getForObject(url, Player.class);
+            players.add(player);
+        }
+
+        return players;
     }
     
 }

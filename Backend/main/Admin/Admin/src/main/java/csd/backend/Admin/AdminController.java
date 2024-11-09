8daa3e37c729f2e88eeeeb8180c717e9601cfdc5
Rachel YJ <rachelyj14@gmail.com>
2024-11-09 12:37:02 +0800

    players crud

--- Backend/main/Admin/Admin/src/main/java/csd/backend/Admin/AdminController.java
+++ Backend/main/Admin/Admin/src/main/java/csd/backend/Admin/AdminController.java
@@ -1,5 +1,7 @@
 package csd.backend.Admin;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,9 +12,22 @@ public class AdminController {
     @Autowired
     private AdminService adminService;
 
+    //to do: add jwt
     @PostMapping("/authenticate")
     public String authenticate(@RequestParam String username, @RequestParam String password) {
         return adminService.authenticateUser(username, password);
+    }
+    @GetMapping("/getAllUsers")
+    public List<User> getAllUsers() {
+        return adminService.getAllUsers();
+    }
+    @PostMapping("/createUser")
+    public String createUser(@RequestParam String username, @RequestParam String password) {
+        return adminService.createUser(username, password);
+    }
+    @DeleteMapping("/deleteUser")
+    public String deleteUser(@RequestParam String username) {
+        return adminService.deleteUser(username);
     }
 }
 
--- Backend/main/Admin/Admin/src/main/java/csd/backend/Admin/AdminService.java
+++ Backend/main/Admin/Admin/src/main/java/csd/backend/Admin/AdminService.java
@@ -1,5 +1,7 @@
 package csd.backend.Admin;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +15,29 @@ public class AdminService {
         return userRepository.findByUsernameAndPassword(username, password)
                 .map(user -> "user authorized")
                 .orElse("user unauthorized");
+    }
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+    public String createUser(String username, String password) {
+
+        if (userRepository.existsById(username)) {
+            return "User already exists";
+        }
+
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);  // to-do: hash the password
+        userRepository.save(user);
+        
+        return "User created successfully";
+    }
+    public String deleteUser(String username) {
+        if (userRepository.existsById(username)) {
+            userRepository.deleteById(username);
+            return "User deleted successfully";
+        } else {
+            return "User not found";
+        }
     }
 }

378f33f22e33551663a4687b53aa88d09d29d69e
denise <22denise.lie@gmail.com>
2024-11-11 15:41:31 +0800

    delete test

--- Backend/main/Account MS/Account-MS/src/test/java/csd/backend/Account/MS/Account_MS/AccountMsApplicationTests.java
+++ Backend/main/Account MS/Account-MS/src/test/java/csd/backend/Account/MS/Account_MS/AccountMsApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AccountMsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }

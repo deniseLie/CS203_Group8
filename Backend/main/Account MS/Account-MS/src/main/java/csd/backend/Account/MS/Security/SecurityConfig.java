531327fbc7b8bb97be0ff865c54b6cbdfddd61e4
Ryan <ryantanshaoen987@gmail.com>
2024-11-13 06:12:56 +0800

    Fixed account

--- Backend/main/Account MS/Account-MS/src/main/java/csd/backend/Account/MS/Security/SecurityConfig.java
+++ Backend/main/Account MS/Account-MS/src/main/java/csd/backend/Account/MS/Security/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 // import org.springframework.security.config.http.SessionCreationPolicy;
 // import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 // import org.springframework.security.crypto.password.PasswordEncoder;
@@ -30,6 +31,10 @@ public class SecurityConfig {
 
     @Autowired
     private JwtRequestFilter jwtRequestFilter;
+
+    public SecurityConfig(JwtRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {

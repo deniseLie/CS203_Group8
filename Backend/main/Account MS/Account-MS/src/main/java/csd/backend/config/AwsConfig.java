3bffc13c4b52c8bdc8d43aeb777acda4e04e3b4a
denise <22denise.lie@gmail.com>
2024-11-12 17:14:27 +0800

    fix aws configs

--- Backend/main/Account MS/Account-MS/src/main/java/csd/backend/config/AwsConfig.java
+++ Backend/main/Account MS/Account-MS/src/main/java/csd/backend/config/AwsConfig.java
@@ -1,6 +1,6 @@
 package csd.backend.config;
 
-import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
@@ -11,11 +11,14 @@ import software.amazon.awssdk.services.sqs.SqsClient;
 @Configuration
 public class AwsConfig {
 
-    private final Dotenv dotenv = Dotenv.load();
-
-    private final String accessKey = dotenv.get("AWS_ACCESS_KEY_ID");
-    private final String secretKey = dotenv.get("AWS_SECRET_ACCESS_KEY");
-    private final String region = dotenv.get("AWS_REGION");
+    @Value("${aws.access-key-id}")
+    private String accessKey;
+    
+    @Value("${aws.secret-access-key}")
+    private String secretKey;
+    
+    @Value("${aws.region}")
+    private String region;
 
     @Bean
     public SqsClient sqsClient() {

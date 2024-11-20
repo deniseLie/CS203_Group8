6392e64dff355c8014a5c6421c16d5c54bd1e7d2
denise <22denise.lie@gmail.com>
2024-11-20 17:36:29 +0800

    database connected

--- Backend/main/Admin/Admin/src/main/java/csd/backend/Admin/AdminApplication.java
+++ Backend/main/Admin/Admin/src/main/java/csd/backend/Admin/AdminApplication.java
@@ -16,8 +16,11 @@ public class AdminApplication {
 		System.setProperty("ACCOUNT_QUEUE_URL", dotenv.get("ACCOUNT_QUEUE_URL"));
 		System.setProperty("MATCHMAKING_QUEUE_URL", dotenv.get("MATCHMAKING_QUEUE_URL"));
 		System.setProperty("PENALTY_QUEUE_URL", dotenv.get("PENALTY_QUEUE_URL"));
-		System.setProperty("ADMIN_QUEUE_URL", dotenv.get("ADMIN_QUEUE_URL"));
 		System.setProperty("LOGIN_QUEUE_URL", dotenv.get("LOGIN_QUEUE_URL"));
+		System.setProperty("ADMIN_QUEUE_URL", dotenv.get("ADMIN_QUEUE_URL"));
+		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
 
 		SpringApplication.run(AdminApplication.class, args);
 	}
